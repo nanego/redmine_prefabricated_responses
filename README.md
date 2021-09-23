0dda80d187208b6378e618a5ae9e043a21a01516
Vincent Robert <vincent.robert@nanego.com>
2021-09-23 10:36:47 +0200

    Prepare new plugin

---
+++ README.md
@@ -0,0 +1,33 @@
+Redmine Prefabricated Responses
+======================
+
+
+
+## Installation
+
+Note that this plugin has a required dependency. That's why you will also have to install this "technical" plugin:
+* **redmine_base_deface** [here](https://github.com/jbbarth/redmine_base_deface)
+
+Please apply general instructions for plugins [here](http://www.redmine.org/wiki/redmine/Plugins).
+
+## Test status
+
+|Plugin branch| Redmine Version   | Test Status      |
+|-------------|-------------------|------------------|
+|master       | 4.2.2             | [![4.2.2][1]][5] |
+|master       | master            | [![master][2]][5]|
+
+[1]: https://github.com/nanego/redmine_prefabricated_responses/actions/workflows/4_2_2.yml/badge.svg
+[2]: https://github.com/nanego/redmine_prefabricated_responses/actions/workflows/master.yml/badge.svg
+[5]: https://github.com/nanego/redmine_prefabricated_responses/actions
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## License
+MIT License.
--- init.rb
+++ init.rb
@@ -1,8 +1,27 @@
 Redmine::Plugin.register :redmine_prefabricated_responses do
-  name 'Redmine Prefabricated Responses plugin'
+  name
   author 'Author name'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+end
+
+require 'redmine_prefabricated_responses/hooks'
+
+ActiveSupport::Reloader.to_prepare do
+  # require_dependency 'redmine_prefabricated_responses/issue_patch'
+end
+
+Redmine::Plugin.register :redmine_prefabricated_responses do
+  name 'Redmine Prefabricated Responses plugin'
+  author 'Vincent ROBERT'
+  description 'This is a plugin for Redmine. It allows you to prepare frequent responses and make easy for your users to use them.'
+  version '1.0.0'
+  url 'https://github.com/nanego/redmine_prefabricated_responses'
+  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.4' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
+  menu :admin_menu, :prefabricated_responses, { :controller => 'prefabricated_responses', :action => 'index' },
+       :caption => :label_prefabricated_responses,
+       :html => {:class => 'icon'}
 end
