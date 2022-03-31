b7140fb7fa0c541e8c1b69db7704d9b91b92a53d
Yazan <y.alaeddin@adacis.net>
2022-03-31 09:57:30 +0200

    ajouter les scopes global_for_project + private_for_user

--- app/models/response.rb
+++ app/models/response.rb
@@ -24,14 +24,40 @@ class Response < ActiveRecord::Base
   scope :not_private, -> { where(is_private: false) }
   scope :sorted, -> { order(:name) }
   scope :visible_by, ->(user) { where("is_private = ? OR author_id = ?", false, user.id) }
-
   before_save :remove_empty_value_from_serialized_field
 
   VISIBILITY_PRIVATE = 0
-  VISIBILITY_ROLES   = 1
   VISIBILITY_PUBLIC  = 2
+  VISIBILITY_ROLES   = 1
 
   validates :visibility, :inclusion => {:in => [VISIBILITY_PUBLIC, VISIBILITY_ROLES, VISIBILITY_PRIVATE]}
+  scope :private_for_user, ->(user) do
+    if user.admin?
+      where("visibility <> ? And author_id = ? AND project_id IS NULL", VISIBILITY_ROLES, user.id)
+    else
+        where("visibility = ? And author_id = ? AND project_id IS NULL", VISIBILITY_PRIVATE, user.id)
+     end
+  end
+
+  def self.global_for_project(user = User.current, project_id)
+    scope = joins("INNER JOIN #{Project.table_name} ON #{table_name}.project_id = #{Project.table_name}.id").
+      where("#{table_name}.project_id = ?", project_id)
+    if user.admin?
+      #scope.where("#{table_name}.visibility <> ?", VISIBILITY_PRIVATE)
+      scope
+    elsif user.memberships.any?
+      scope.where(
+        "author_id = ?" +
+        " OR #{table_name}.visibility IN (?, ?)" +
+        " OR (#{table_name}.visibility = ? AND #{table_name}.id IN (" +
+        "SELECT DISTINCT res.id FROM #{table_name} res" +
+        " INNER JOIN responses_roles res_ro on res_ro.response_id = res.id" +
+        " INNER JOIN #{MemberRole.table_name} mr ON mr.role_id = res_ro.role_id" +
+        " INNER JOIN #{Member.table_name} m ON m.id = mr.member_id AND m.user_id = ?" +
+        " INNER JOIN #{Project.table_name} p ON p.id = m.project_id AND m.project_id = ?))",
+        user.id, VISIBILITY_PRIVATE, VISIBILITY_PUBLIC, VISIBILITY_ROLES, user.id, project_id)
+    end
+  end
 
   def allowed_target_projects
     Project.active
