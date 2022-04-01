4a431405755e10b7d8c99bdc65ee89c8ee080bf4
Yazan <y.alaeddin@adacis.net>
2022-04-01 11:21:12 +0200

    corriger les routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 RedmineApp::Application.routes.draw do
-  get 'responses/new', to: 'responses#new', as: 'new_response'
-  get 'projects/:project_id/responses/new', to: 'responses#new', as: 'project_new_response'
 
-  get 'responses/', to: 'responses#index', as: 'responses'
-  get 'projects/:project_id/responses/', to: 'responses#index', as: 'project_responses'
+  resources :projects do
+    resources :responses, :only => [:index, :new, :create]
+  end
 
-  get 'response/:id', controller: 'responses', action: 'show', as: 'show_response'
-  resources :responses, :except => [:new, :index, :show] do
+  resources :responses do
     collection do
       post :add
       post :apply
       post :update_note
     end
   end
+
 end
