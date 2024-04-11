3fcf86796a57427bf2a0a3133317135a1b65440f
Yalaeddin <y.alaeddin@adacis.net>
2024-04-11 16:42:31 +0200

    Ajouter le patch ProjectPatch

--- lib/redmine_prefabricated_responses/project_patch.rb
+++ lib/redmine_prefabricated_responses/project_patch.rb
@@ -1,7 +1,12 @@
 require_dependency 'project'
 
+module RedminePrefabricatedResponses
+  module ProjectPatch
+
+  end
+end
+
 class Project < ActiveRecord::Base
-
   has_many :responses, :dependent => :destroy
-
+  prepend RedminePrefabricatedResponses::ProjectPatch
 end
