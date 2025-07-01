a11370459ff65757c9813d77ec2419740c44b53b
Vincent <contact@vincent-robert.com>
2025-07-01 18:47:59 +0200

    Add missing indexes

---
+++ db/migrate/20250701160618_add_missing_indexes.rb
@@ -0,0 +1,10 @@
+class AddMissingIndexes < ActiveRecord::Migration[7.2]
+  def change
+    add_index :responses, :assigned_to_id, if_not_exists: true
+    add_index :responses, :author_id, if_not_exists: true
+    add_index :responses, :final_status_id, if_not_exists: true
+    add_index :responses, :project_id, if_not_exists: true
+    add_index :responses_roles, :role_id, if_not_exists: true
+    add_index :responses_roles, [:response_id, :role_id], if_not_exists: true
+  end
+end
